597ba56aaf7dcf738255e1624ee548ade0a63b93
Justin Le Tourneau <justin.letourn@gmail.com>
2019-12-29 07:30:42 -0600

    changing public path for socket.io

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 // vue.config.js
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/software/knightsvsminions/' : '/',
+	//publicPath: process.env.NODE_ENV === 'production' ? '/software/knightsvsminions/' : '/',
+	publicPath: process.env.PUBLIC_PATH
 }
