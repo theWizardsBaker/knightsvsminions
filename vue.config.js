f2f785df4344165a84f4d42023cd2b2ff7aae5b3
Justin Le Tourneau <justin.letourn@gmail.com>
2019-12-30 11:52:45 -0600

    changing publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js
 module.exports = {
 	//publicPath: process.env.NODE_ENV === 'production' ? '/software/knightsvsminions/' : '/',
-	publicPath: process.env.PUBLIC_PATH
+	// publicPath: process.env.PUBLIC_PATH
+	publicPath: './'
 }
